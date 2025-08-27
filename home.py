c4c3492c01da84ac476ad83c4f274aee84883619
LSLeal14 <lucassoaresleal@ufu.br>
2025-08-27 09:32:12 -0300

    processamento dos dados das tabelas de planejamento e medição

--- home.py
+++ home.py
@@ -6,3 +6,22 @@ def main():
 
     st.title("Início")
     st.write("Bem-vindo!")
+
+    st.header("Passo a passo para uso da plataforma")
+    st.subheader("1. Navegação")
+
+
+
+    st.subheader("2. Cadastro de Projeto")
+
+
+
+    st.subheader("3. Consulta e geração do relatório de medição")
+
+
+
+    st.subheader("4. Atualização de tabela de Planejamento")
+
+
+
+    st.subheader("5. Atualização de tabela de Medição")
